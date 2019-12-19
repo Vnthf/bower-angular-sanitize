36bded424577b768f2206c18ff22c3a911e5ea88
최민호 <vnthf1@naver.com>
2019-12-19 12:52:56 +0900

    Update angular-sanitize.js
    allow block element 에 summary,details 추가

--- angular-sanitize.js
+++ angular-sanitize.js
@@ -252,7 +252,7 @@
 // Safe Block Elements - HTML5
     var blockElements = angular.extend({}, optionalEndTagBlockElements, toMap("address,article," +
         "aside,blockquote,caption,center,del,dir,div,dl,figure,figcaption,footer,h1,h2,h3,h4,h5," +
-        "h6,header,hgroup,hr,ins,map,menu,nav,ol,pre,section,table,ul"));
+        "h6,header,hgroup,hr,ins,map,menu,nav,ol,pre,section,table,ul,summary,details"));
 
 // Inline Elements - HTML5
     var inlineElements = angular.extend({}, optionalEndTagInlineElements, toMap("a,abbr,acronym,b," +
